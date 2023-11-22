7f9dfcb3ee4822d79f795537e32a21bad4364bd3
jciminiatures <122825996+jciminiatures@users.noreply.github.com>
2023-11-22 15:54:55 +0000

    Add files via upload

--- generateGalleryIMG.py
+++ generateGalleryIMG.py
@@ -1,6 +1,6 @@
 import os
 import json
-files = os.listdir('.\\images\\gallery\\')
+files = os.listdir(os.path.expanduser('~/Desktop/website-gallery/images/gallery'))
 x = {"gallery": files}
 y = json.dumps(x)
 
