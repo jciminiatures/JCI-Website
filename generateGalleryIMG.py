a367e07ceb49663cffd04325cd5bba5cf97b0a56
jciminiatures <122825996+jciminiatures@users.noreply.github.com>
2023-11-27 12:54:28 +0000

    fixed silly mac problem

--- generateGalleryIMG.py
+++ generateGalleryIMG.py
@@ -2,6 +2,7 @@ import os
 import json
 files = os.listdir(os.path.expanduser('~/Desktop/website-gallery/images/gallery'))
 x = {"gallery": files}
+x['gallery'].remove('.DS_Store')
 y = json.dumps(x)
 
 with open('galleryIMG.json','w') as f:
