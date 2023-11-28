c1bdd90bac46f30c0c26229116af076abf47ab79
jciminiatures <122825996+jciminiatures@users.noreply.github.com>
2023-11-28 13:33:28 +0000

    Add files via upload

--- generateGalleryIMG.py
+++ generateGalleryIMG.py
@@ -2,7 +2,10 @@ import os
 import json
 files = os.listdir(os.path.expanduser('~/Desktop/website-gallery/images/gallery'))
 x = {"gallery": files}
-x['gallery'].remove('.DS_Store')
+try:
+    x['gallery'].remove('.DS_Store')
+except:
+    pass
 y = json.dumps(x)
 
 with open('galleryIMG.json','w') as f:
