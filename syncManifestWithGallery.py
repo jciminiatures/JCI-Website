5a3244f196b2001edf83e4a022d7b8c6428f1ec7
Samuel Horner <samkoseyhorner@gmail.com>
2025-01-17 17:11:32 +0000

    Updated sync manifest with gallery script to log changes

--- syncManifestWithGallery.py
+++ syncManifestWithGallery.py
@@ -3,9 +3,12 @@ import json
 
 gallery_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images", "gallery")
 
+with open("manifest.json","r") as f:
+    old_manifest = json.loads(f.read())["gallery"]
+
 with open("manifest.json","w") as f:
     files = os.listdir(gallery_path)
-    
+
     # MACOS bypass
     try:
         files.remove(".DS_Store")
@@ -13,4 +16,11 @@ with open("manifest.json","w") as f:
         pass
 
     f.write(json.dumps({"gallery": files}))
+    
     print("[OK]: Finished syncing manifest to gallery.")
+    
+    removed = [file for file in old_manifest if file not in files]
+    added = [file for file in files if file not in old_manifest]
+    print("[CHANGES]:")
+    for i in added: print(f"  + {i}")
+    for i in removed: print(f"  - {i}")
